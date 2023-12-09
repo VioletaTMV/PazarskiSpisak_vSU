3443cee64d0475134b6911e22f1dbcab39ae7c54
Violeta <vilcho@yahoo.com>
2023-12-10 01:02:52 +0200

    deleted unused links, templates, classes

--- PazarskiSpisak_v_SU/src/main/java/com/pazarskispisak/PazarskiSpisak/init/MigratingDataFromOldDBAndEncryptPasswords.java
+++ PazarskiSpisak_v_SU/src/main/java/com/pazarskispisak/PazarskiSpisak/init/MigratingDataFromOldDBAndEncryptPasswords.java
@@ -1,36 +1,36 @@
-package com.pazarskispisak.PazarskiSpisak.init;
-
-import com.pazarskispisak.PazarskiSpisak.service.SeedService;
-import com.pazarskispisak.PazarskiSpisak.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-@Component
-@Order(1)
-public class MigratingDataFromOldDBAndEncryptPasswords implements CommandLineRunner {
-
-    private final SeedService seedService;
-    private final PasswordEncoder passwordEncoder;
-    private final UserService userService;
-
-    @Autowired
-    public MigratingDataFromOldDBAndEncryptPasswords(SeedService seedService, PasswordEncoder passwordEncoder, UserService userService) {
-        this.seedService = seedService;
-        this.passwordEncoder = passwordEncoder;
-        this.userService = userService;
-    }
-
-
-    @Override
-    public void run(String... args) throws Exception {
-
-            //migrating data
-         this.seedService.seedDatabase();
-
-
-    }
-}
+//package com.pazarskispisak.PazarskiSpisak.init;
+//
+//import com.pazarskispisak.PazarskiSpisak.service.SeedService;
+//import com.pazarskispisak.PazarskiSpisak.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Component
+//@Order(1)
+//public class MigratingDataFromOldDBAndEncryptPasswords implements CommandLineRunner {
+//
+//    private final SeedService seedService;
+//    private final PasswordEncoder passwordEncoder;
+//    private final UserService userService;
+//
+//    @Autowired
+//    public MigratingDataFromOldDBAndEncryptPasswords(SeedService seedService, PasswordEncoder passwordEncoder, UserService userService) {
+//        this.seedService = seedService;
+//        this.passwordEncoder = passwordEncoder;
+//        this.userService = userService;
+//    }
+//
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//            //migrating data
+//         this.seedService.seedDatabase();
+//
+//
+//    }
+//}
--- PazarskiSpisak_v_SU/src/main/java/com/pazarskispisak/PazarskiSpisak/models/dtos/IngredientQtyAndMeasureForRecipe.java
+++
@@ -1,4 +0,0 @@
-//package com.pazarskispisak.PazarskiSpisak.models.dtos;
-//
-//public record IngredientQtyAndMeasureForRecipe() {
-//}
